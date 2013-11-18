c6e4627ac00f90598e433a3525ab6c3d7bbfe861
Kyle Rippey <kyle@avvo.com>
2013-11-17 22:29:48 -0800

    Removed Card class for now since it is unused.

--- lib/cah.rb
+++ lib/cah.rb
@@ -2,7 +2,6 @@ module Cah
   # TODO: Move this to a config file
   CARDS_IN_HAND = 10
 
-  autoload :Card, 'cah/card'
   autoload :Deck, 'cah/deck'
   autoload :Game, 'cah/game'
   autoload :Player, 'cah/player'
